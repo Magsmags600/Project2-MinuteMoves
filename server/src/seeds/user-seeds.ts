4c2ec6e6f279738b93d8e6b88add186929c76a25
larimae <larissamae60@gmail.com>
2024-10-25 14:16:17 -0700

    generated more users and their data

--- server/src/seeds/user-seeds.ts
+++ server/src/seeds/user-seeds.ts
@@ -23,6 +23,55 @@ export const seedUsers = async () => {
         email: 'radiant@comet.com',
         password: 'password',
       },
+      {
+        firstName: 'Luna',
+        lastName: 'Johnson',
+        username: 'LunarWanderer',
+        email: 'luna@wanderer.com',
+        password: 'password',
+      },
+      {
+        firstName: 'Ethan',
+        lastName: 'Blake',
+        username: 'BlazingHawk',
+        email: 'ethan@hawk.com',
+        password: 'password',
+      },
+      {
+        firstName: 'Olivia',
+        lastName: 'Morris',
+        username: 'OceanBreeze',
+        email: 'olivia@breeze.com',
+        password: 'password',
+      },
+      {
+        firstName: 'Ava',
+        lastName: 'Davis',
+        username: 'StellarDreamer',
+        email: 'ava@dreamer.com',
+        password: 'password',
+      },
+      {
+        firstName: 'Liam',
+        lastName: 'Smith',
+        username: 'NebulaKnight',
+        email: 'liam@knight.com',
+        password: 'password',
+      },
+      {
+        firstName: 'Maya',
+        lastName: 'Reed',
+        username: 'CosmicMuse',
+        email: 'maya@muse.com',
+        password: 'password',
+      },
+      {
+        firstName: 'Zara',
+        lastName: 'Patel',
+        username: 'GalacticSoul',
+        email: 'zara@soul.com',
+        password: 'password',
+      }
     ],
     { individualHooks: true }
   );
