2db8ea4ebe59a74d550865a3af9ef86bd51bcf72
Izaac Ramirez <izaacramirez@Izaacs-Laptop.local>
2024-10-27 12:45:45 -0700

    edited the user model and the routes for user problems

--- server/src/routes/api/user-routes.ts
+++ server/src/routes/api/user-routes.ts
@@ -35,9 +35,9 @@ router.get('/:id', async (req: Request, res: Response) => {
 
 // POST api/users - Create a new user
 router.post('/', async (req: Request, res: Response) => {
-  const { username, email, password } = req.body;
+  const { firstName, lastName, username, email, password } = req.body;
   try {
-    const newUser = await User.create({ username, email, password });
+    const newUser = await User.create({ firstName, lastName, username, email, password });
     res.status(201).json(newUser);
   } catch (error: any) {
     res.status(400).json({ message: error.message });
