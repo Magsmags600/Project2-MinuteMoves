af041d1f632e11fa098e5cf402ae543d2171f1f4
larimae <larissamae60@gmail.com>
2024-10-27 11:46:07 -0700

    imported css file to the MealPage file

--- client/src/pages/MealPage.tsx
+++ client/src/pages/MealPage.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { RecipeData } from "../interfaces/RecipeData"; // Define this to structure your recipe info
 import { fetchRecipes } from "../api/recipeAPI"; // API call to get recipes based on targets
+import './css/MealPage.css';
 
 const MealPage = () => {
     // State for recommended recipes and nutrient goals
