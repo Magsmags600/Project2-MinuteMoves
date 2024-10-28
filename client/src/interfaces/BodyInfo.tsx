c44f18a3bb0c08fc3554921cc54c411a4da2fac4
Izaac Ramirez <izaacramirez@Izaacs-Laptop.local>
2024-10-28 11:07:58 -0700

    added the bodyInfo interface for front-end

---
+++ client/src/interfaces/BodyInfo.tsx
@@ -0,0 +1,12 @@
+export interface BodyInfo {
+    id: number;
+    calories: number;
+    protein: number;
+    carbs: number;
+    fat: number;
+    percentage: number;
+    userId: number;
+    createdAt: Date;
+    updatedAt: Date;
+  }
+  
